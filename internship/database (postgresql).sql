0fdf74f59c1cd9048bafc720d443642beabdd685
Intoxikot <netoxikot@gmail.com>
2022-04-21 14:03:04 +0500

    Исправления и доработки
    Добавлены последовательности, изменены типы данных, зависимости включены в таблицы

--- internship/database (postgresql).sql
+++ internship/database (postgresql).sql
@@ -1,63 +1,115 @@
-CREATE TABLE "store.product" (
-	"product_id" serial NOT NULL,
-	"name" varchar NOT NULL,
-	CONSTRAINT "product_pk" PRIMARY KEY ("product_id")
-) WITH (
-  OIDS=FALSE
+-- CREATE DATABASE store; 
+
+DROP TABLE IF EXISTS store.product;
+DROP TABLE IF EXISTS store.income;
+DROP TABLE IF EXISTS store.outcome;
+DROP TABLE IF EXISTS store.document;
+DROP TABLE IF EXISTS store.inventory_item;
+
+/*
+CREATE SEQUENCE store.product_id_seq;
+CREATE SEQUENCE store.income_id_seq;
+CREATE SEQUENCE store.outcome_id_seq;
+CREATE SEQUENCE store.document_id_seq;
+CREATE SEQUENCE store.inventory_item_id_seq;
+*/
+
+CREATE TABLE store.product (
+	product_id int NOT NULL 
+	DEFAULT NEXTVAL('store.product_id_seq'::regclass),
+	name character varying NOT NULL,
+	CONSTRAINT product_pk 
+		PRIMARY KEY (product_id)
 );
 
-CREATE TABLE "store.income" (
-	"income_id" serial NOT NULL,
-	"product_id" integer NOT NULL,
-	"document_id" integer NOT NULL,
-	"count" FLOAT NOT NULL,
-	"cost" FLOAT NOT NULL,
-	CONSTRAINT "income_pk" PRIMARY KEY ("income_id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE store.document (
+	document_id int NOT NULL
+	DEFAULT NEXTVAL('store.document_id_seq'::regclass),
+	code character varying NOT NULL UNIQUE,
+	date timestamp without time zone NOT NULL,
+	CONSTRAINT "document_pk" 
+		PRIMARY KEY ("document_id")
 );
 
-CREATE TABLE "store.outcome" (
-	"outcome_id" serial NOT NULL,
-	"product_id" integer NOT NULL,
-	"document_id" integer NOT NULL,
-	"count" serial NOT NULL,
-	"cost" FLOAT NOT NULL,
-	CONSTRAINT "outcome_pk" PRIMARY KEY ("outcome_id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE store.income (
+	income_id bigint NOT NULL
+	DEFAULT NEXTVAL('store.income_id_seq'::regclass),
+	product_id int NOT NULL,
+	document_id int NOT NULL,
+	count numeric(8,4) NOT NULL,
+	cost numeric(16,8) NOT NULL,
+	CONSTRAINT income_pk  
+		PRIMARY KEY (income_id),
+	CONSTRAINT income_fk0
+		FOREIGN KEY (product_id) 
+		REFERENCES store.product (product_id),
+	CONSTRAINT income_fk1 
+		FOREIGN KEY (document_id) 
+		REFERENCES store.document (document_id)
 );
 
-CREATE TABLE "store.document" (
-	"document_id" serial NOT NULL,
-	"code" varchar NOT NULL UNIQUE,
-	"date" DATETIME NOT NULL,
-	CONSTRAINT "document_pk" PRIMARY KEY ("document_id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE store.outcome (
+	outcome_id bigint NOT NULL
+	DEFAULT NEXTVAL('store.outcome_id_seq'::regclass),
+	product_id int NOT NULL,
+	document_id int NOT NULL,
+	count numeric(8,4) NOT NULL,
+	cost numeric(16,8) NOT NULL,
+	CONSTRAINT outcome_pk
+		PRIMARY KEY (outcome_id),
+	CONSTRAINT outcome_fk0
+		FOREIGN KEY (product_id) 
+		REFERENCES store.product (product_id),
+	CONSTRAINT outcome_fk1
+		FOREIGN KEY (document_id) 
+		REFERENCES store.document (document_id)
 );
 
-CREATE TABLE "store.inventory_item" (
-	"storage_id" serial NOT NULL,
-	"document_id" integer NOT NULL,
-	"product_id" integer NOT NULL,
-	"count" FLOAT NOT NULL,
-	CONSTRAINT "inventory_item_pk" PRIMARY KEY ("storage_id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE store.inventory_item (
+	"item_id" bigint NOT NULL
+	DEFAULT NEXTVAL('store.inventory_item_id_seq'::regclass),
+	"document_id" int NOT NULL,
+	"product_id" int NOT NULL,
+	"count" numeric(8,4) NOT NULL,
+	CONSTRAINT "inventory_item_id_pk" 
+		PRIMARY KEY ("item_id"),
+	CONSTRAINT inventory_item_fk0 
+		FOREIGN KEY (document_id) 
+		REFERENCES store.document (document_id),
+	CONSTRAINT inventory_item_fk1
+		FOREIGN KEY (product_id) 
+		REFERENCES store.product(product_id)
 );
 
-ALTER TABLE "income" ADD CONSTRAINT "income_fk0" FOREIGN KEY ("product_id") REFERENCES "product"("product_id");
-ALTER TABLE "income" ADD CONSTRAINT "income_fk1" FOREIGN KEY ("document_id") REFERENCES "document"("document_id");
 
-ALTER TABLE "outcome" ADD CONSTRAINT "outcome_fk0" FOREIGN KEY ("product_id") REFERENCES "product"("product_id");
-ALTER TABLE "outcome" ADD CONSTRAINT "outcome_fk1" FOREIGN KEY ("document_id") REFERENCES "document"("document_id");
+/* Внешние ключи отдельно (уже включены в таблицы)
+ALTER TABLE store.income 
+ADD CONSTRAINT income_fk0
+FOREIGN KEY (product_id) 
+REFERENCES store.product (product_id);
 
+ALTER TABLE store.income 
+ADD CONSTRAINT income_fk1 
+FOREIGN KEY (document_id) 
+REFERENCES store.document (document_id);
 
-ALTER TABLE "inventory_item" ADD CONSTRAINT "inventory_item_fk0" FOREIGN KEY ("document_id") REFERENCES "document"("document_id");
-ALTER TABLE "inventory_item" ADD CONSTRAINT "inventory_item_fk1" FOREIGN KEY ("product_id") REFERENCES "product"("product_id");
+ALTER TABLE store.outcome 
+ADD CONSTRAINT outcome_fk0
+FOREIGN KEY (product_id) 
+REFERENCES store.product (product_id);
 
+ALTER TABLE store.outcome 
+ADD CONSTRAINT outcome_fk1
+FOREIGN KEY (document_id) 
+REFERENCES store.document (document_id);
 
+ALTER TABLE store.inventory_item 
+ADD CONSTRAINT inventory_item_fk0 
+FOREIGN KEY (document_id) 
+REFERENCES store.document (document_id);
 
-
-
+ALTER TABLE store.inventory_item  
+ADD CONSTRAINT inventory_item_fk1
+FOREIGN KEY (product_id) 
+REFERENCES storeproduct (product_id);
+*/
