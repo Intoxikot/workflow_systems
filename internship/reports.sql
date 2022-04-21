a00d1d459c725b3789934103f89594d24ccfb9f1
Intoxikot <netoxikot@gmail.com>
2022-04-21 22:52:38 +0500

    Update reports.sql

--- internship/reports.sql
+++ internship/reports.sql
@@ -1,5 +1,4 @@
--- Учет по остаткам для каждого товара
-
+-- Учет по остаткам для каждого товара (по ид)
 SELECT
 	fluidity.product_id,
 	sum(fluidity.count) as count
@@ -28,8 +27,47 @@ GROUP BY
 ORDER BY
 	fluidity.product_id
 
--- Учет по бухгалтерскому движению средств по месяцам
+	
+-- Учет по остаткам для каждого товара (с именем товара)
+SELECT 
+	product.product_id,
+	product.name,
+	remind.count
+FROM
+	store.product
+JOIN
+	(SELECT
+		fluidity.product_id,
+		SUM(fluidity.count) as count
+	FROM
+		store.document,
+		(SELECT 
+			income.product_id, 
+			income.document_id, 
+			income.count
+		FROM 
+			store.income
+		UNION ALL
+		SELECT 
+			outcome.product_id, 
+			outcome.document_id, 
+			(outcome.count * -1) as count
+		FROM 
+			store.outcome
+		) as fluidity
+	WHERE
+		document.document_id = fluidity.document_id and
+		document.document_id = fluidity.document_id 
+		/* and document.date >= {BeginDate} and document.date <= {FinalDate} */
+	GROUP BY
+		fluidity.product_id
+	ORDER BY
+		fluidity.product_id) as remind
+ON 
+	remind.product_id = product.product_id
 
+	
+-- Учет по бухгалтерскому движению средств по месяцам
 SELECT
 	to_char(document.date :: DATE, 'yyyy/mm') as date,
 	sum(fluidity.income) as income
