ee6920a49a92b1e30f202babc65e16ef72eeb124
Intoxikot <netoxikot@gmail.com>
2022-04-21 22:18:03 +0500

    Update test_data.sql

--- internship/test_data.sql
+++ internship/test_data.sql
@@ -6,7 +6,7 @@ INSERT INTO store.document (code, date) VALUES ('Отгрузка 1', '2022.02.1
 INSERT INTO store.document (code, date) VALUES ('Отгрузка 2', '2022.03.14 14:00');
 
 INSERT INTO store.income (document_id, product_id, count, cost) VALUES (1, 1, 10, 1000);
-INSERT INTO store.income (document_id, product_id, count, cost) VALUES (2, 1, 1, 1000);
+INSERT INTO store.income (document_id, product_id, count, cost) VALUES (2, 1, 1, 1200);
 
 INSERT INTO store.outcome (document_id, product_id, count, cost) VALUES (3, 1, 3, 800);
 INSERT INTO store.outcome (document_id, product_id, count, cost) VALUES (4, 1, 7, 1500);
