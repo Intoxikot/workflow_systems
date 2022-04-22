7d5d38724b6be4718c64d304f2812d3a6f7b50b9
Intoxikot <netoxikot@gmail.com>
2022-04-22 15:55:45 +0500

    Отчеты обновлены для новой схемы

--- internship/reports.sql
+++ internship/reports.sql
@@ -3,25 +3,30 @@ SELECT
 	fluidity.product_id,
 	SUM(fluidity.count) as count
 FROM
-	store.document,
 	(SELECT 
-		income.product_id, 
-		income.document_id, 
-		income.count
+		income_item.product_id, 
+		income_item.count,
+		income.date
 	FROM 
 		store.income
+	JOIN
+		store.income_item
+	ON
+		income.document_id = income_item.document_id
 	UNION ALL
 	SELECT 
-		outcome.product_id, 
-		outcome.document_id, 
-		(outcome.count * -1) as count
+		outcome_item.product_id, 
+		outcome_item.count * -1,
+		outcome.date
 	FROM 
 		store.outcome
+	JOIN
+		store.outcome_item
+	ON
+		outcome.document_id = outcome_item.document_id
 	) as fluidity
-WHERE
-	document.document_id = fluidity.document_id and
-	document.document_id = fluidity.document_id 
-	/* and document.date >= {BeginDate} and document.date <= {FinalDate} */
+/* WHERE
+	fluidity.date >= {BeginDate} and fluidity.date <= {FinalDate} */
 GROUP BY
 	fluidity.product_id
 ORDER BY
@@ -41,31 +46,37 @@ JOIN
 		fluidity.product_id,
 		SUM(fluidity.count) as count
 	FROM
-		store.document,
 		(SELECT 
-			income.product_id, 
-			income.document_id, 
-			income.count
+			income_item.product_id, 
+			income_item.count,
+			income.date
 		FROM 
 			store.income
+		JOIN
+			store.income_item
+		ON
+			income.document_id = income_item.document_id
 		UNION ALL
 		SELECT 
-			outcome.product_id, 
-			outcome.document_id, 
-			(outcome.count * -1) as count
+			outcome_item.product_id, 
+			outcome_item.count * -1,
+			outcome.date
 		FROM 
 			store.outcome
-		) as fluidity
-	WHERE
-		document.document_id = fluidity.document_id and
-		document.document_id = fluidity.document_id 
-		/* and document.date >= {BeginDate} and document.date <= {FinalDate} */
+		JOIN
+			store.outcome_item
+		ON
+			outcome.document_id = outcome_item.document_id
+		) AS fluidity
+	/* WHERE
+		fluidity.date >= {BeginDate} and fluidity.date <= {FinalDate} */
 	GROUP BY
 		fluidity.product_id
-	ORDER BY
-		fluidity.product_id) as remind
-ON 
-	remind.product_id = product.product_id
+	) AS remind
+ON
+	product.product_id = remind.product_id
+ORDER BY
+	product.product_id
 
 	
 -- Учет по бухгалтерскому движению средств по месяцам
